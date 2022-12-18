7d7c66920313a27a91113246fe1e88b847feae0c
Ryo Ota <nwtgck@nwtgck.org>
2022-12-18 15:41:37 +0900

    browser version output

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # vue-e2e-multi-playwright
 
-
-```
-npm ci
-TS_NODE_COMPILER_OPTIONS='{"module":"commonjs"}' ./node_modules/.bin/ts-node tests/e2e/test.ts 
-```
+See .github/workflows.ci.yml to know hot to run.
--- e2e-test/src/index.ts
+++ e2e-test/src/index.ts
@@ -1,18 +1,23 @@
 import * as assert from 'power-assert';
+
+// chromium 93.0.4576.0, firefox 90.0, webkit 14.2
 import * as playwright_1_13 from "playwright-1-13";
+// chromium 109.0.5414.46, firefox 107.0, webkit 16.4
 import * as playwright_1_29 from "playwright-1-29";
 
 
 describe('playwright', () => {
   it('goes to page without error', async () => {
     for (const playwright of [playwright_1_13, playwright_1_29]) {
+      const out = [];
       for (const browserType of ['chromium', 'firefox', 'webkit'] as const) {
         const browser = await playwright[browserType].launch();
         const page = await browser.newPage();
         await page.goto("https://example.com");
-        console.log(browserType, browser.version());
+        out.push(`${browserType} ${browser.version()}`);
         await browser.close();
       }
+      console.log(out.join(", "));
     }
   });
 });
